bbd515661b3a6aa9d2e4279107437a815dc14ff2
naisenyamungai <naisenyamungai@gmail.com>
2019-07-20 16:41:31 +0300

    add logic

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 'use strict';
 
-const
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+const apiai = require('apiai');
+const express = require('express');
+const config = require('./config');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('webhook is listening');
-});
+if(!config.FB_PAGE_TOKEN){
+    throw new Error('missing FB_PAGE_TOKEN');
+}
+if (!config.FB_VERIFY_TOKEN) {
+	throw new Error('missing FB_VERIFY_TOKEN');
+}
+if (!config.SERVER_URL) { //used for ink to static files
+	throw new Error('missing SERVER_URL');
+}
+
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+// app.listen(process.env.PORT || 4000, function(){
+//     console.log('webhook is listening');
+// });
 
 app.get('/', function (req,res){
     res.send("Hello world, I am a chatBot")
 })
 
 app.post('/webhook', (req, res) => {
-    let body = req.body;
+    var data = req.body;
+    console.log(JSON.stringify(data));
 
-    if(body.object === 'page'){
+    if(data.object === 'page'){
 
-        body.entry.forEach(function(entry){
+        data.entry.forEach(function(pageEntry){
 
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
@@ -31,20 +51,16 @@ app.post('/webhook', (req, res) => {
     }
 })
 
-app.get('/webhook', (req, res) => {
-
-    let VERIFY_TOKEN = "myvoiceisnotmypassword"
-
-    let mode = req.quert['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+app.get('/webhook', function (req, res) {
+    console.log("request");
 
     if(mode && token){
 
-        if (mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
+        if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN){
+            res.status(200).send(req.query['hub.challenge']);
+        
         } else{
+            console.error("Failed validation. Make sure the validation tokens match.");
             res.sendStatus(403);
         }
     }
