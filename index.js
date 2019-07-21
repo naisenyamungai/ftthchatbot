8d7aa6a58e0c87eb58cb42a3d8488ba8a9f2f7df
naisenyamungai <naisenyamungai@gmail.com>
2019-07-21 12:46:55 +0300

    add get response

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.post('/', function(req, res){
     if(req.body.result.action == "payment"){
         var paymentmethods = req.body.result.parameters.methods;
         console.log(methods);
+
+        var response = "";
+
+        if(methods == "mpesa"){
+            response = "Dial *400# and follow procedure";
+        }else{
+            response = "No";
+        }
+        res.json({
+            "displayText": response
+        })
+
     }
 
 })
@@ -25,37 +37,37 @@ app.listen(process.env.PORT || 4000);
 //     res.send("Hello I am a chatbot")
 // })
 
-app.post('/webhook', (req, res) => {
-    let body = req.body;
+// app.post('/webhook', (req, res) => {
+//     let body = req.body;
 
-    if(body.object === 'page'){
+//     if(body.object === 'page'){
 
-        body.entry.forEach(function(entry){
+//         body.entry.forEach(function(entry){
 
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-        res.status(200).send('EVENT_RECEIVED');
-    } else{
-        res.sendStatus(404);
-    }
-})
+//             let webhook_event = entry.messaging[0];
+//             console.log(webhook_event);
+//         });
+//         res.status(200).send('EVENT_RECEIVED');
+//     } else{
+//         res.sendStatus(404);
+//     }
+// })
 
-app.get('/webhook', (req, res) => {
+// app.get('/webhook', (req, res) => {
 
-    let VERIFY_TOKEN = "myvoiceisnotmypassword"
+//     let VERIFY_TOKEN = "myvoiceisnotmypassword"
 
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+//     let mode = req.query['hub.mode'];
+//     let token = req.query['hub.verify_token'];
+//     let challenge = req.query['hub.challenge'];
 
-    if(mode && token){
+//     if(mode && token){
 
-        if (mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else{
-            res.sendStatus(403);
-        }
-    }
-})
+//         if (mode === 'subscribe' && token === VERIFY_TOKEN){
+//             console.log('WEBHOOK_VERIFIED');
+//             res.status(200).send(challenge);
+//         } else{
+//             res.sendStatus(403);
+//         }
+//     }
+// })
