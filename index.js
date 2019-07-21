52aa44ca76aedf909c434d6d94a3a8909b52069a
naisenyamungai <naisenyamungai@gmail.com>
2019-07-21 08:07:53 +0300

    modify logic

--- index.js
+++ index.js
@@ -13,41 +13,41 @@ app.post('/', function(req, res){
 
 app.listen(process.env.PORT || 4000, () => console.log('webhook is listening'));
 
-app.get('/', function (req,res){
-    res.send("Hello I am a chatbot")
-})
+// app.get('/', function (req,res){
+//     res.send("Hello I am a chatbot")
+// })
 
-app.post('/webhook', (req, res) => {
-    let body = req.body;
+// app.post('/webhook', (req, res) => {
+//     let body = req.body;
 
-    if(body.object === 'page'){
+//     if(body.object === 'page'){
 
-        body.entry.forEach(function(entry){
+//         body.entry.forEach(function(entry){
 
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-        res.status(200).send('EVENT_RECEIVED');
-    } else{
-        res.sendStatus(404);
-    }
-})
+//             let webhook_event = entry.messaging[0];
+//             console.log(webhook_event);
+//         });
+//         res.status(200).send('EVENT_RECEIVED');
+//     } else{
+//         res.sendStatus(404);
+//     }
+// })
 
-app.get('/webhook', (req, res) => {
+// app.get('/webhook', (req, res) => {
 
-    let VERIFY_TOKEN = "myvoiceisnotmypassword"
+//     let VERIFY_TOKEN = "myvoiceisnotmypassword"
 
-    let mode = req.quert['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+//     let mode = req.quert['hub.mode'];
+//     let token = req.query['hub.verify_token'];
+//     let challenge = req.query['hub.challenge'];
 
-    if(mode && token){
+//     if(mode && token){
 
-        if (mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else{
-            res.sendStatus(403);
-        }
-    }
-})
+//         if (mode === 'subscribe' && token === VERIFY_TOKEN){
+//             console.log('WEBHOOK_VERIFIED');
+//             res.status(200).send(challenge);
+//         } else{
+//             res.sendStatus(403);
+//         }
+//     }
+// })
