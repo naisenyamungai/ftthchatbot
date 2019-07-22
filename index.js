930213ee1f029de7cf8049ee17887c62044c31cb
naisenyamungai <naisenyamungai@gmail.com>
2019-07-22 14:47:04 +0300

    add logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 const
     express = require('express'),
@@ -11,24 +11,24 @@ app.use(bodyParser.json());
 app.post('/', function(req, res){
     console.log(req.body)
 
-    // if(req.body.queryResult.action == "payment"){
+    if(req.body.queryResult.action == "payment"){
 
-    //     console.log("Payment Action");
-    //     var method = req.body.queryResult.parameters.method;
-    //     console.log(method);
+        console.log("Payment Action");
+        var method = req.body.queryResult.parameters.method;
+        console.log(method);
 
-    //     var response = "";
+        var response = "";
 
-    //     if(method == "method"){
-    //         response = "Dial *400# and follow procedure";
-    //     }else{
-    //         response = "No";
-    //     }
-    //     res.json({
-    //         "displayText": response
-    //     })
+        // if(method == "method"){
+        //     response = "Dial *400# and follow procedure";
+        // }else{
+        //     response = "No";
+        // }
+        // res.json({
+        //     "displayText": response
+        // })
 
-    // }
+    }
 
 })
 
@@ -40,37 +40,37 @@ app.listen(process.env.PORT || 4000);
 //     res.send("Hello I am a chatbot")
 // })
 
-app.post('/webhook', (req, res) => {
-    let body = req.body;
+// app.post('/webhook', (req, res) => {
+//     let body = req.body;
 
-    if(body.object === 'page'){
+//     if(body.object === 'page'){
 
-        body.entry.forEach(function(entry){
+//         body.entry.forEach(function(entry){
 
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-        res.status(200).send('EVENT_RECEIVED');
-    } else{
-        res.sendStatus(404);
-    }
-})
+//             let webhook_event = entry.messaging[0];
+//             console.log(webhook_event);
+//         });
+//         res.status(200).send('EVENT_RECEIVED');
+//     } else{
+//         res.sendStatus(404);
+//     }
+// })
 
-app.get('/webhook', (req, res) => {
+// app.get('/webhook', (req, res) => {
 
-    let VERIFY_TOKEN = "myvoiceisnotmypassword"
+//     let VERIFY_TOKEN = "myvoiceisnotmypassword"
 
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+//     let mode = req.query['hub.mode'];
+//     let token = req.query['hub.verify_token'];
+//     let challenge = req.query['hub.challenge'];
 
-    if(mode && token){
+//     if(mode && token){
 
-        if (mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else{
-            res.sendStatus(403);
-        }
-    }
-})
+//         if (mode === 'subscribe' && token === VERIFY_TOKEN){
+//             console.log('WEBHOOK_VERIFIED');
+//             res.status(200).send(challenge);
+//         } else{
+//             res.sendStatus(403);
+//         }
+//     }
+// })
