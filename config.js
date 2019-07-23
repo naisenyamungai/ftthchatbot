edb872fbab176fafb4afe78a80a746336846ff55
naisenyamungai <naisenyamungai@gmail.com>
2019-07-23 14:53:52 +0300

    add logic

--- config.js
+++
@@ -1,4 +0,0 @@
-export const API_AI_CLIENT_ACCESS_TOKEN = '1a6a5f427dcf44a9bfa221b933110146';
-export const SERVER_URL = "https://ftth-bot.herokuapp.com/";
-// export const FB_PAGE_TOKEN = 'EAAGHE0MqZCY0BAJoo40ZA6eae2Xl4KtwMtoVaZCrSFOtKO3aeGj2guRbeyiqwhopbsoESEXrbbZBxhoSfDWhZAo8TN8CiDZCTZC9X1SLZAapnsmLsPstQY6yGJ5RGnbRmmADu5e9VZAZBjZBMndVAVxOSZBZBXIBXjSW8FTA3vUOJ8ti2Gbip015ZALl7E';
-// export const FB_VERIFY_TOKEN = 'myvoiceisnotmypassword'
